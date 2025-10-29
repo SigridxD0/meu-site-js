8d1a3b681d90af6abcd5061bea5c2514eeffbdb2
SigridxD0 <sigri.x.d17@gmail.com>
2025-10-29 17:54:17 -0300

    feat: adiciona formulário de login com mensagem de boas-vindas
    - Adiciona formulário de login com campo para nome
    - Implementa função processarLogin para manipular o submit do formulário
    - Adiciona estilização CSS para o formulário e mensagem de boas-vindas
    - Corrige sintaxe e adiciona ponto-e-vírgula em funções JavaScript

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 
 
 function butao_magico() {
-    const elemento = document.getElementById("pa1")
-    elemento.innerHTML = "O js Funcionouu"
+    const elemento = document.getElementById("pa1");
+    elemento.innerHTML = "O js Funcionouu";
 }
 function alternarElemento() {
-    const elemento = document.getElementById("elemento-secreto")
-    elemento.classList.toggle("escondido")
+    const elemento = document.getElementById("elemento-secreto");
+    elemento.classList.toggle("escondido");
+}
+
+const meuFormulario = document.getElementById("form1");
+meuFormulario.addEventListener("submit", processarLogin);
+function processarLogin(event){
+    event.preventDefault();
+    const campoNome = document.getElementById("campo-nome");
+    const nomeDigitado = campoNome.value;
+    const elementomensagem = document.getElementById("boas-vindas");
+    elementomensagem.innerHTML = `Olá ${nomeDigitado}! Seja bem vindo!`;
+    campoNome.value = "";
+
+
 }
