3b86376cb5f8cb78a5d4b2876a82e07a166c6df8
SigridxD0 <sigri.x.d17@gmail.com>
2025-10-29 09:21:21 -0300

    Foi adicionado um botao onde tem a funçao de revelar ou esconder uma div com um titulo e paragrafo

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-const elemento = document.getElementById("pa1")
+
 
 function butao_magico() {
+    const elemento = document.getElementById("pa1")
     elemento.innerHTML = "O js Funcionouu"
+}
+function alternarElemento() {
+    const elemento = document.getElementById("elemento-secreto")
+    elemento.classList.toggle("escondido")
 }
