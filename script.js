9586684736b9c709a4c248d6a2e490684f89d4da
SigridxD0 <sigri.x.d17@gmail.com>
2025-10-29 23:21:21 -0300

    feat: adiciona formulário de login e mensagem de boas-vindas

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const historicoNomes = [];
+const historicoUsuarios = [];
 
 function butao_magico() {
     const elemento = document.getElementById("pa1");
@@ -16,17 +16,38 @@ function processarLogin(event){
     event.preventDefault();
     const campoNome = document.getElementById("campo-nome");
     const nomeDigitado = campoNome.value;
-    historicoNomes.push(nomeDigitado);
-    atualizarlista();
-    campoNome.value = "";
+    const nomeLimpo = nomeDigitado.trim();
+    if (nomeLimpo === "") {
+        campoNome.classList.add("input-erro");
+        alert("Por favor, insira um nome válido.");
+        return;
+    }
+    campoNome.classList.remove("input-erro");
+    const indicator = document.getElementById("loading-indicator");
+    indicator.classList.remove("escondido");
+    const botaoForm = meuFormulario.querySelector("button[type='submit']");
+    botaoForm.disabled = true;
+    botaoForm.textContent = "Carregando...";
+    botaoForm.style.cursor = "not-allowed";    
+    setTimeout(() => {    
+        const novoUsuario = { nome: nomeDigitado, dataEntrada: new Date() };
+        historicoUsuarios.push(novoUsuario);
+        atualizarlista();
+        campoNome.value = "";
+        indicator.classList.add("escondido");
+        botaoForm.disabled = false;
+        botaoForm.textContent = "Entrar";
+        botaoForm.style.cursor = "pointer";
+    }, 2000);
+
 }
 function atualizarlista(){
     const listaNomes = document.getElementById("lista-historico");
     
     listaNomes.innerHTML = "";
-    for (const nome of historicoNomes){
+    for (const usuario of historicoUsuarios){
         const itemLista = document.createElement("li");
-        itemLista.textContent = nome;
+        itemLista.textContent = `${usuario.nome} logou em ${usuario.dataEntrada.toLocaleString()}`;
         listaNomes.appendChild(itemLista);
     }
 }
